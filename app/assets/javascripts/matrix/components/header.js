8b9958e1f02700ca51a1513fc23d9c0bc88a0afd
De Nayer <denayer.nicolas@gmail.com>
2016-11-07 22:22:08 +0100

    feature(filter): init tag filter

--- app/assets/javascripts/matrix/components/header.js
+++ app/assets/javascripts/matrix/components/header.js
@@ -1,13 +1,18 @@
 import React from 'react'
-import FilterLink from '../containers/filter_link.js'
+import FilterLink from '../components/filter_link.js'
 
-const Header = () => (
+const Header = ({ tags = []}) => (
 
   <div className='row man pan'>
     <div className='col-md-5'>
       <FilterLink filter="SHOW_ALL">All</FilterLink>&nbsp;
       <FilterLink filter="SHOW_ACTIVE">Active</FilterLink>&nbsp;
-      <FilterLink filter="SHOW_COMPLETED">Completed</FilterLink >&nbsp;
+      <FilterLink filter="SHOW_COMPLETED">Completed</FilterLink>&nbsp;
+      {
+        tags.map( tag =>
+          <FilterLink filter={tag}>{tag}</FilterLink>
+        )
+      }
     </div>
     <div className='col-md-5 h3 maxs hidden-xs hidden-sm'>
       {matrix_infos.name}
